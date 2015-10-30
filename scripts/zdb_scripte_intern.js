cedde34a303bda38a2e5be72dc6e75abb2d4acd7
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2015-10-30 11:22:03 +0100

    aktiviere Nutzerscripte

--- scripts/zdb_scripte_intern.js
+++ scripts/zdb_scripte_intern.js
@@ -1,5 +1,19 @@
 // Datei:		zdb_scripte_intern.js
-
+/**
+ * checks if a user script file is already configured . If not a file is created and path configured
+ * this function is called on startup
+ * */
+ __enableUserScriptFile();
+function __enableUserScriptFile(){
+    var loadandSave = application.getProfileString("ibw.userscript.location","loadandSave","");
+        if(loadandSave == "")
+        {
+            var language = application.getProfileString("ibw.userscript","language","JS").toLowerCase();
+            var theOutput = utility.newFileOutput();
+            var fileOutput = theOutput.createSpecial("ProfD", "winibw." + language);
+            theOutput.close();
+    }
+}
 /**
  * zählt Exemplardatensätze
  * 
