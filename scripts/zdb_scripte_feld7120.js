b17f8084fb362a4e4e48eb4f7121c9d8dcf75e33
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2015-11-16 15:09:13 +0100

    Besseres regex für eckige Klammern

--- scripts/zdb_scripte_feld7120.js
+++ scripts/zdb_scripte_feld7120.js
@@ -226,7 +226,7 @@ function __Klammern7120(feld) {
     // Eckige Klammern mit Inhalt: Muster, Bindestrich, Blank weglassen
     klammern7120 = klammern7120.replace(/\[\d{4}]- |\[\d{4}\/\d\d]- /, " -");
     // Eckige Klammern mit Inhalt: Muster am Feldende weglassen
-    klammern7120 = klammern7120.replace(/\[\d{4}]$|\[\d{4}\/\d\d]$/, "");
+    klammern7120 = klammern7120.replace(/\[\d{4}]|\[\d{4}\/\d\d]/, "");
 
     // Eckige Klammern entfernen
     klammern7120 = klammern7120.replace(/\[/gi, "");
