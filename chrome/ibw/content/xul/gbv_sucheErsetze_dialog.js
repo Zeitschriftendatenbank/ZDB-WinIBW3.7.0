80d495c5c5e90551388dfb1ca6764f3b1febc224
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2016-08-30 07:49:37 +0200

    bProtokoll immer false für neue Aktion

--- chrome/ibw/content/xul/gbv_sucheErsetze_dialog.js
+++ chrome/ibw/content/xul/gbv_sucheErsetze_dialog.js
@@ -236,6 +236,7 @@ function bearbeiteSetErsetzen() {
 
         //Falls Aktion erneut gestartet (z.B. nach Fehlermeldung)
         lBearbeitet = 0;
+        bProtokoll = false
         verbergeSchlussmeldung(true);
 
         strSuche = document.getElementById("idSuche").value;
@@ -412,6 +413,7 @@ function bearbeiteSetZufuegen() {
 
         //Falls Aktion erneut gestartet (z.B. nach Fehlermeldung)
         lBearbeitet = 0;
+        bProtokoll = false
         verbergeSchlussmeldung(true);
 
         if (wennKat.length != dannKat.length) {
