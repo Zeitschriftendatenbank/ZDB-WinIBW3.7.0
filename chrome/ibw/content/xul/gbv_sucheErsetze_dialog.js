a1acdb410ea3d1f69015fd889c9221888700f338
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2019-11-28 09:01:31 +0100

    added user 6666

--- chrome/ibw/content/xul/gbv_sucheErsetze_dialog.js
+++ chrome/ibw/content/xul/gbv_sucheErsetze_dialog.js
@@ -112,8 +112,8 @@ function onLoad() {
         hinweisVZG = "Sie haben Kategorien der bibliographischen Ebene (Titelebene) ausgewählt. " +
             "\nDas Bearbeiten ganzer Sets auf bibliographischer Ebene ist der Zentralredaktion vorbehalten." +
             "\nBitte senden Sie Ihre Korrekturvorschläge an zdb-winibw@sbb.spk-berlin.de";
-        strELN = "8007|8009|9001|9006|9002";
-        strUser = "6001|6199|6099|6004|6207";
+        strELN = "8007|8009|9001|9006|9002|6666";
+        strUser = "6001|6005|6199|6099|6004|6207";
         strKatLok = "2080|348[01]|354[0-9]|471[056]|476[34]|60[0-9xX][0-9xX]|6100|65[0-9xX][0-9xX]";
         strKatExe = "480[012]|4820|4822|6700|70[0-9xX][0-9xX]|710[0-9]|7120|713[345678]|714[0-9]|715[09]|7[89]00|8001|803[12345]|8[12]00|844[89]|846[567]|8510|859[45678]";
         break;
